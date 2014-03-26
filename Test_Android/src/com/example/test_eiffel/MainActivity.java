e99259ccc50752329ce936e456f0171f2a7b3dc0
Louis M <prog@tioui.com>
2014-03-25 23:03:48 -0400

    Bug in the Android project

--- Test_Android/src/com/example/test_eiffel/MainActivity.java
+++ Test_Android/src/com/example/test_eiffel/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.test_eiffel;
 
+import com.eiffel.EiffelActivity;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Eiffel_Activity {
+public class MainActivity extends EiffelActivity {
 
 	
 
